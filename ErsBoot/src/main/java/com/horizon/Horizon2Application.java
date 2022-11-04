b0bb03ca56094cc22f46c34ddf25f79270214b74
btayalew <bayalew@gmail.com>
2022-11-04 09:42:42 -0500

    Changed datasource url

--- ErsBoot/src/main/java/com/horizon/Horizon2Application.java
+++ ErsBoot/src/main/java/com/horizon/Horizon2Application.java
@@ -3,6 +3,7 @@ package com.horizon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
